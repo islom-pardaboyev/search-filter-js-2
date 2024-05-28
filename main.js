c9f2055e02b9f4a3705ad5358f9881ddd708728b
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-05-28 15:46:49 +0500

    init

---
+++ main.js
@@ -0,0 +1,37 @@
+const postsCon = document.querySelector('#postsCon');
+const searchInput = document.querySelector('#searchInput');
+
+let posts = [];
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase();
+    posts.forEach(post => {
+        const isVisible =
+            post.id.toString().includes(value) ||
+            post.title.toLowerCase().includes(value) ||
+            post.body.toLowerCase().includes(value);
+        post.element.classList.toggle("hidden", !isVisible);
+    });
+});
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(resp => resp.json())
+    .then(data => {
+        posts = data.map(post => {
+            const contentDiv = document.createElement('div');
+            contentDiv.classList.add('col-span-4');
+            contentDiv.innerHTML = `
+                    <p>ID: ${post.id}</p>
+                    <h1>Title: ${post.title}</h1>
+                    <p>Body: ${post.body}</p>
+                    `;
+
+            postsCon.appendChild(contentDiv);
+
+            post.element = contentDiv
+
+            return post;
+        });
+        console.log(data);
+    })
+    .catch(err => console.log(err));
